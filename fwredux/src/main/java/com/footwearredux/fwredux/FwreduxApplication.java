b197bbc6dcdff42361bc136529bee7edd11de3f8
catalin cot <catalincot15@gmail.com>
2023-11-03 13:46:36 +0200

    [Create-springboot-project] Generated spring boot  project

---
+++ fwredux/src/main/java/com/footwearredux/fwredux/FwreduxApplication.java
@@ -0,0 +1,13 @@
+package com.footwearredux.fwredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwreduxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwreduxApplication.class, args);
+	}
+
+}
