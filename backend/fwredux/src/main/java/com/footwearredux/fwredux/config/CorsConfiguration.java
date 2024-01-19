405731c91adfbb11b56cec357d43772df8ad6c2c
catalin cot <catalincot15@gmail.com>
2024-01-19 05:40:38 +0200

    [JsFetch] Added remove/fetch cart

--- backend/fwredux/src/main/java/com/footwearredux/fwredux/config/CorsConfiguration.java
+++ backend/fwredux/src/main/java/com/footwearredux/fwredux/config/CorsConfiguration.java
@@ -13,7 +13,9 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("POST", "GET", "DELETE");
             }
         };
     }
